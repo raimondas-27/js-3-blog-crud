970fef160b00d451f10be4a42e6b4540c9ee8c20
raimondas-27 <raimondasvilnius@yahoo.com>
2021-05-31 09:33:55 +0300

    index, bootstrap, navbar pages html

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const path = require("path");
+
 
 const app = express();
-
 const PORT = 5000;
 
 app.get("/", function (req, res) {
-   res.send("hello world");
+   res.sendFile(path.join(__dirname, "pages", "index.html"))
 });
+
+app.get("/about", function (req, res) {
+   res.sendFile(path.join(__dirname, "pages", "about.html"))
+});
+
+app.get("/blog", function (req, res) {
+   res.sendFile(path.join(__dirname, "pages", "blog.html"))
+});
+
+//statine direktorija css,js,img ir kitiems statiniams failams
+const staticPath = path.join(__dirname, "static")
+app.use(express.static(staticPath));
 
 //404 case
 
