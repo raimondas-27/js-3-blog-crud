2e53fc2fd0378a9721c2079f501f190c627508a6
raimondas-27 <raimondasvilnius@yahoo.com>
2021-05-31 09:48:07 +0300

    added readme.md file, ejs install, html to ejs, render pages.ejs

---
+++ README.md
@@ -0,0 +1,6 @@
+install ejs
+npm install ejs
+and register
+//register view engine
+app.set("view engine","ejs");
+pakeisti folder structure
--- src/index.js
+++ src/index.js
@@ -1,20 +1,33 @@
 const express = require("express");
 const path = require("path");
 
-
 const app = express();
 const PORT = 5000;
 
+
+//register view engine
+
+app.set("view engine", "ejs");
+
+//render views homedir
+
+app.set("views", "src/views");
+
+
 app.get("/", function (req, res) {
-   res.sendFile(path.join(__dirname, "pages", "index.html"))
+   // res.sendFile(path.join(__dirname, "pages", "index.js"))
+   //paimti index.ejs faila is views direktorijos
+   res.render("index")
 });
 
 app.get("/about", function (req, res) {
-   res.sendFile(path.join(__dirname, "pages", "about.html"))
+   // res.sendFile(path.join(__dirname, "pages", "about.js"))
+   res.render("about")
 });
 
 app.get("/blog", function (req, res) {
-   res.sendFile(path.join(__dirname, "pages", "blog.html"))
+   // res.sendFile(path.join(__dirname, "pages", "blog.js"))
+   res.render("blog")
 });
 
 //statine direktorija css,js,img ir kitiems statiniams failams
